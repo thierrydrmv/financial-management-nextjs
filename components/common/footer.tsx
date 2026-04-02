94f4d9ce7befe646c0a0d162913eab25abe4c77e
Thierry de Ridder Martins Varela <varelathierry@gmail.com>
2026-04-02 19:23:40 -0300

    fix: footer text

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
-    <footer className="order-t py-12">
-      <div className="wrapper">Wealth Tracker Inc. All rights reserved.</div>
+    <footer className="border-t border-border/40 py-12">
+      <div className="wrapper">
+        <p className="text-center">
+          © {new Date().getFullYear()} Wealth Tracker Inc.
+        </p>
+      </div>
     </footer>
   );
 }
